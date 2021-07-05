c52dcb9664f503a06002eb011318c4f1b3a228db
devFerzen <52632495+devFerzen@users.noreply.github.com>
2021-07-04 20:04:40 -0500

    Base front-end para comportamientos

---
+++ src/components/Panel-Herramientas.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    :loading="herramientasLoader"
+    class="ml-2 ml-md-6 mt-2"
+    height="85%"
+    >
+    <v-card-title> Lorem ipsum dolor sit amet</v-card-title>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Panel-Herramientas',
+    data: () => ({
+      herramientasLoader: false,
+    }),
+  }
+</script>
+
+<style lang="scss">
+  .loginClass {
+    background-color: red;
+  }
+
+  .herramientasHigh {
+    height: 100%;
+  }
+</style>
---
+++ src/components/Sistema-Tarjetas.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    Sistema Tarjetas
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Sistema-tarjetas',
+    data: () => ({
+
+    }),
+  }
+</script>
