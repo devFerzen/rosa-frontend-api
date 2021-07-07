758007396afef87fa112ca4395af0a1bd76f06d7
Alan Ferzen Secenas Siller <alan.secenas@PEXPRESS.LOCAL>
2021-07-07 11:33:21 -0500

    errors of git

--- src/App.vue
+++ src/App.vue
@@ -30,11 +30,11 @@
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-        text
-      >
+        text>
         <span class="mr-2">Latest Release</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
+      
     </v-app-bar>
 
     <v-main>
