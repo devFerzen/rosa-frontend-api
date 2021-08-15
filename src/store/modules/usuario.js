9ded9b399a6068a866f5beeed948831212517973
devFerzen <52632495+devFerzen@users.noreply.github.com>
2021-08-15 15:36:33 -0500

    host8

--- src/store/modules/usuario.js
+++ src/store/modules/usuario.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://suspicious-carson-4cf004.netlify:3000`,
   withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
