b5db29105c1427ef8641d1fae19d56aef986a2a7
devFerzen <52632495+devFerzen@users.noreply.github.com>
2022-06-28 19:46:48 -0500

    http protocol

--- src/mixins/Test/generalTest-mixin.js
+++ src/mixins/Test/generalTest-mixin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://localhost:3000',
   headers:{
         'Access-Control-Allow-Origin': '*',
         'Accept': 'application/json'
