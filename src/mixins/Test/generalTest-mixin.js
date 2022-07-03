fdd83ae2000cc675891c750ba26ee679efce89fc
devFerzen <52632495+devFerzen@users.noreply.github.com>
2022-07-03 10:12:35 -0500

    'fixErr_Connection_Refuse2'

--- src/mixins/Test/generalTest-mixin.js
+++ src/mixins/Test/generalTest-mixin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "http://localhost:3080/api",
+  baseURL: "https://localhost:3080/api",
   headers: {
     "Access-Control-Allow-Origin": "https://error-609.herokuapp.com", //"http://localhost:8080"
     "Accept": "application/json",
