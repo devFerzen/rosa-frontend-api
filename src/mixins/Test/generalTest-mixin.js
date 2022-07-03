9be21fd80f1720652190f81138cc7f32081c9669
devFerzen <52632495+devFerzen@users.noreply.github.com>
2022-07-02 22:04:27 -0500

    'server_url_ready2'

--- src/mixins/Test/generalTest-mixin.js
+++ src/mixins/Test/generalTest-mixin.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "http://localhost:3080/api",
   headers: {
-    "Access-Control-Allow-Origin": "https://error-609.herokuapp.com",
+    "Access-Control-Allow-Origin": "http://localhost:8080", //"*" "https://error-609.herokuapp.com",
     "Accept": "application/json",
-    "Access-Control-Allow-Methods": "GET, POST, DELETE",
+    "Access-Control-Allow-Methods": "GET, POST, DELETE"
   },
 });
 
