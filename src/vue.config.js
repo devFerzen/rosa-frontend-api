fbdc40830d0e24ad2fe18309e164f20f3c2144e4
devFerzen <52632495+devFerzen@users.noreply.github.com>
2022-07-03 10:32:08 -0500

    'fixErr_Connection_Refuse4'

--- src/vue.config.js
+++ src/vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
   //Error socket localhost -> notes in https://stackoverflow.com/questions/55754906/errors-in-browser-console-requests-to-sockjs-node-infot-1555629946494
   devServer: {
     proxy: {
-      "^/api": {
-        target: "https://localhost:3080/api",
+      "/api": {
+        target: "https://localhost:3080" //"https://error-609.herokuapp.com:3080/api", //"https://localhost:3080/api",
       },
     },
   },
